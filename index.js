a7b1ce26f76a0eeec161a04930737b50449da1f2
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2024-06-29 16:24:13 +0700

    :+1: Setup logger with morgan

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+var morgan = require("morgan");
+
 const express = require("express");
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", function (req, res) {
   res.send("Hello World");
