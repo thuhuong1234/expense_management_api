126fbce9185bdd095a67706ce7aa6dcb2a2783e8
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2024-06-29 16:18:30 +0700

    :+1: Setup env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+const port = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(port);
