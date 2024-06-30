9e062a291fc57032bffad01942e5342993801d34
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2024-06-30 22:43:27 +0700

    :+1: Add express.json()

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
-const errorHandler = require("./src/middlewares/error.middleware");
-
-var morgan = require("morgan");
-
 const express = require("express");
-const app = express();
+const router = require("./src/routers/index");
+const errorHandler = require("./src/middlewares/error.middleware");
+const morgan = require("morgan");
 
-app.use(morgan("dev"));
+const app = express();
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
+
 app.use(errorHandler);
 
+app.use("/api/v1", router);
+
 app.listen(port, () => {
-  console.log(`Server run with port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
