45ab7ce0ff1e02e1a6cc64f45d0d6eff2a2e9fbc
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2024-07-03 17:12:38 +0700

    :alien: Handles authorization for APIs

--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware");
+const roleMiddleware = require("../middlewares/role.middleware");
+
 const categoryRouter = express.Router();
 
 const categoryController = require("../controllers/category.controller");
 
 categoryRouter.route("/").get(categoryController.getAllCategories);
+
+categoryRouter.use(authMiddleware, roleMiddleware(true));
+
 categoryRouter.route("/").post(categoryController.createCategory);
 categoryRouter.route("/:id").get(categoryController.getCategory);
 categoryRouter.route("/:id").put(categoryController.updateCategory);
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,8 +1,13 @@
 const userController = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+const roleMiddleware = require("../middlewares/role.middleware");
+
 const router = require("express").Router();
 
-router.route("/").post(userController.createUser);
 router.route("/").get(userController.getAllUsers);
+
+router.use(authMiddleware).use(roleMiddleware(true));
+router.route("/").post(userController.createUser);
 router.route("/:id").get(userController.getUser);
 router.route("/:id").patch(userController.updateUser);
 router.route("/:id").delete(userController.deleteUser);
