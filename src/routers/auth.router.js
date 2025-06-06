b71de790f5f5d0e262346177ecbddd1259400034
thuhuong1234 <hdnguyenthuhuong1234@gmail.com>
2025-06-06 09:38:07 +0700

    :+1: Add register router

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,8 +1,10 @@
 const authControllers = require("../controllers/auth.controller");
+const userControllers = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const router = require("express").Router();
 
 router.route("/login").post(authControllers.login);
+router.route("/register").post(userControllers.createUser);
 router.route("/forgot-password").post(authControllers.forgotPassword);
 router.route("/reset-password").post(authControllers.resetPassword);
 router.use(authMiddleware).route("/user").get(authControllers.getUser);
